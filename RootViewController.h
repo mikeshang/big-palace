c48b2e1d1602f700a2cf15d53cf12732fb3e7e50
mikeshang <mike_0_shang@sina.com>
2015-02-16 10:12:28 +0800

    RootViewController.h
    INITIAL style

---
+++ RootViewController.h
@@ -0,0 +1,9 @@
+/* sample of initial RootViewController */
+
+#import “Foundation/foundation.h"
+#import "UIKit/UIKit/h"
+
+@interface RootViewController ；UITableViewController ｛
+                   NSDate *timestamp；｝    /*RootViewController  类的局部变量*/
+@property （nonatomic，retain）NSDate *timestamp；
+@end
