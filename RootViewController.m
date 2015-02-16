cf2aa78552711f1c775bbfcd0f6da3ce7283e242
mike shang <mike_0_shang@sina.com>
2015-02-16 13:04:44 +0800

    Update RootViewController.m

--- RootViewController.m
+++ RootViewController.m
@@ -7,11 +7,19 @@
 
 @implementation RootViewController
 
-@synthesize timestamp = _timestamp;
+@synthesize timestamp = _timestamp;   /*_实例对象  _timestamp加下划线了，可以区别属性timestamp*/
 
+- (NSDate *)timestamp {
+        return _timestamp;
+        }
+- (void)setTimestamp:(NSDate *)newValue {
+    if (_timestamp != newValue) {
+            [_timestamp = release];            /*这就是属性中self. 调用set方法的意思，先release掉_timestamp中可能有的东西，在retain，RC+1*/
+             _timestamp = [newValue retain];
+            }
 - (void)viewDidload {
-            [super viewDidload];
-            self.timestamp =[NSDate date]; 
+            [super viewDidload];   /*super指的是RootViewController这个类，该类型继承了父类ViewController的ViewDidload方法*/
+            self.timestamp =[NSDate date]; /*调用属性timestamp的setTimestamp方法，【NSDate date】date发送消息给NSDate，放回该类型数值类似newValue*/
             }
 
 - (void)viewWillappear : (BOOL)animated {
@@ -19,7 +27,7 @@
             [self.tableView reloadData];
             }
     
-- (void)ViewDidload {
+- (void)ViewDidUnload {
             [super viewDidload];
             self.timestamp = nil;
             }
